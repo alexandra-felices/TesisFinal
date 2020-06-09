1b4fef7ae3d9cf6ca4fcbe7c08338b27f2ca7818
alexandra-felices <64100164+alexandra-felices@users.noreply.github.com>
2020-06-09 00:41:34 -0500

    Add files via upload

--- data/configuracion.php
+++ data/configuracion.php
@@ -8,8 +8,8 @@
 
 
 
-define("SERVIDOR_BD", "localhost");
-define("USUARIO_BD", "postgres");
-define("CLAVE_BD", "123");
+define("SERVIDOR_BD", "ec2-52-0-155-79.compute-1.amazonaws.com");
+define("USUARIO_BD", "jjttfmgbstinyk");
+define("CLAVE_BD", "6b842899a239c8409829adec04bc51e2bdf3ea4b4fc4989713ea71b4f851ffe0");
 define("PUERTO_BD", "5432");
-define("NOMBRE_BD", "TESIS2020");
+define("NOMBRE_BD", "d137sl95cr4342");
